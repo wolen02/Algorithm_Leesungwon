dd246a709781fb093d7abbfd07e891e2050d2a1b
lsw02 <126543515+lsw02@users.noreply.github.com>
2023-03-01 23:33:33 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a / b);
+	}
+}
