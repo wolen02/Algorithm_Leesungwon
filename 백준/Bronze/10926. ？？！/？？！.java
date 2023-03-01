737a9ba14e6d64d940490340cdfef5f9cb2fcce5
lsw02 <126543515+lsw02@users.noreply.github.com>
2023-03-02 00:10:24 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String name;
+		name = sc.nextLine();
+		System.out.println(name + "??!");
+	}
+}
